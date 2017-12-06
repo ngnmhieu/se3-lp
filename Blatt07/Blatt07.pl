236fd4ffcbaee7adb1dfc15f07b7901a0f17a84f
Hieu Nguyen <ngnmhieu@gmail.com>
2017-12-06 15:12:37 +0100

    Aufgabe 1.1

--- Blatt07/Blatt07.pl
+++ Blatt07/Blatt07.pl
@@ -1,7 +1,13 @@
 % Aufgabe 1.1
-zugriffpfad(0, []).
-zugriffpfad(DirId, ZP) :- 
-directory(1,root,0,date(2007,5,2),date(2007,5,2)).
+% Gibt eine Liste der Verzeichnisnamen auf dem Zugriffspfad zurueck
+% zugriffspfad(+DirId, -Zugriffspfad)
+?- consult('dateiverzeichnis.pl').
+zugriffspfad_rek(0, Acc, Acc).
+zugriffspfad_rek(DirId, Acc, ZP) :-
+  directory(DirId,DirName,PId,_,_),
+  append(Acc, [DirName], NeuAcc),
+  zugriffspfad_rek(PId, NeuAcc, ZP).
+zugriffspfad(DirId, ZP) :- zugriffspfad_rek(DirId, [], ZP).
 
 % Aufgabe 1.2
 
