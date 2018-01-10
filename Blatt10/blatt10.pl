c7c12d859ec0f07f11de3333fd392d4cdd0b9915
Hieu Nguyen <ngnmhieu@gmail.com>
2018-01-10 16:56:40 +0100

    Aufgabe 1

--- Blatt10/blatt10.pl
+++ Blatt10/blatt10.pl
@@ -2,8 +2,22 @@
 % ?- consult('dax2017.pl').
 ?- consult('display_pi.pl').
 
-% TODO: sort nicht nach string sondern nach echte datum
-sort_dax(DAX) :- findall((Datum, X1, X2), dax(Datum, X1, X2), List), sort(1, @<, List, DAX).
+% Aufgabe 1
+
+to_timestamp([Day,Month,Year],Timestamp) :-
+  string_concat(Year,'-',Year_),
+  string_concat(Year_,Month,Year_Month),
+  string_concat(Year_Month,'-',Year_Month_),
+  string_concat(Year_Month_,Day,Year_Month_Day),
+  parse_time(Year_Month_Day, 'iso_8601', Timestamp).
+string_to_timestamp(DatumStr,Timestamp) :-
+  split_string(DatumStr, ".", "", DatumTeile),
+  to_timestamp(DatumTeile,Timestamp).
+dax_with_real_date(Datum, X1, X2, Timestamp) :- dax(Datum,X1,X2), string_to_timestamp(Datum,Timestamp).
+
+sort_dax(DAX) :-
+  findall((Timestamp, X1, X2), dax_with_real_date(Datum, X1, X2, Timestamp), List),
+  sort(1, @<, List, DAX).
 
 get_eroeffnung((_,X1,_),NeuX1) :- NeuX1 is X1 / 500 - 7.
 get_schluss((_,_,X2),NeuX2) :- NeuX2 is X2 / 500 - 7.
@@ -17,3 +31,5 @@ draw_dax(DAX_Daten) :-
   maplist(get_schluss,DAX,DAX_Schluss),
   zip(DAX_Eroeffnung, DAX_Schluss, DAX_Daten),
   display('Zeitreihe', DAX_Daten).
+
+% Aufgabe 2
