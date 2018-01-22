44130a0960f653abdd07d20c859d595bbc012b61
NicoHahn <nicohahn@live.de>
2018-01-22 20:36:36 +0100

    Faktensammlung angefertigt

---
+++ Blatt11/Uebersetzungen.pl
@@ -0,0 +1,30 @@
+%Fakten für die Übersetzung:
+
+uebersetze(er, he).
+uebersetze(sie, she).
+uebersetze(es, it).
+uebersetze(der, the).
+uebersetze(die, the).
+uebersetze(das, the).
+uebersetze(wir, we).
+uebersetze(ihr, you).
+uebersetze(du, you).
+uebersetze(von, of).
+
+uebersetze(leer, empty).
+uebersetze(haus, house).
+uebersetze(beruf, job).
+uebersetze(tier, animal).
+uebersetze(mensch, human).
+uebersetze(auto, car).
+uebersetze(fahrrad, bicycle).
+
+uebersetze(springen, jump).
+uebersetze(laufen, run).
+uebersetze(essen, eat).
+uebersetze(schlafen, sleep).
+uebersetze(spielen, play).
+uebersetze(nett, nice).
+uebersetze(sitzen, sit).
+uebersetze(stehen, stand).
+
